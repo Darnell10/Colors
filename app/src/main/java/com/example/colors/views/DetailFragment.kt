e65573bb1accf778f004d64885b9d550a56f6924
Darnell10 <DarnellOtto@ac.c4q.nyc>
2019-11-02 18:49:16 -0400

    added action to navicgation folder, add databinding to xml views, add onclick fiunctionality to ColorlistAdapter

--- app/src/main/java/com/example/colors/views/DetailFragment.kt
+++ app/src/main/java/com/example/colors/views/DetailFragment.kt
@@ -9,13 +9,18 @@ import android.view.ViewGroup
 import androidx.lifecycle.AndroidViewModel
 
 import com.example.colors.R
+import com.example.colors.viewmodel.DetailViewModel
 
 /**
  * A simple [Fragment] subclass.
  */
 class DetailFragment : Fragment() {
 
+    private lateinit var viewModel: DetailViewModel
+
     private var colorUuid = 0
+
+    private lateinit var dataBinding: Fragment
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
