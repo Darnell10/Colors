2e39e2d9a0dbe0b9b18e320b889fd159fb7ace84
Darnell10 <DarnellOtto@ac.c4q.nyc>
2019-11-02 17:44:35 -0400

    add logic to list fragment

--- app/src/main/java/com/example/colors/views/ListFragment.kt
+++ app/src/main/java/com/example/colors/views/ListFragment.kt
@@ -6,13 +6,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
 
 import com.example.colors.R
+import com.example.colors.viewmodel.ListViewModel
+import kotlinx.android.synthetic.main.fragment_list.*
 
 /**
  * A simple [Fragment] subclass.
  */
 class ListFragment : Fragment() {
+
+    private lateinit var viewModel: ListViewModel
+
+    private val colorListAdapter = ColorListAdapter(arrayListOf())
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,5 +32,58 @@ class ListFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_list, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProviders.of(this).get(ListViewModel::class.java)
+        viewModel.refresh()
+
+        recycler_view.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = colorListAdapter
+        }
+
+        refresh_layout.setOnRefreshListener {
+            recycler_view.visibility = View.GONE
+            listError.visibility = View.GONE
+            loadingView.visibility = View.VISIBLE
+            viewModel.refreshBypassCash()
+            refresh_layout.isRefreshing = false
+        }
+
+        observerViewModel()
+
+    }
+
+    fun observerViewModel() {
+
+        viewModel.colors.observe(this, Observer { colors ->
+            colors?.let {
+                recycler_view.visibility = View.VISIBLE
+                colorListAdapter.updateColorList(colors)
+            }
+
+        })
+
+        viewModel.colorLoadError.observe(this, Observer { isError ->
+            isError?.let {
+                listError.visibility = if (it) View.VISIBLE else View.GONE
+            }
+
+        })
+
+
+        viewModel.loading.observe(this, Observer { isLoading ->
+            isLoading?.let {
+                loadingView.visibility = if (it) View.VISIBLE else View.GONE
+                if (it) {
+                    listError.visibility = View.GONE
+                    recycler_view.visibility = View.GONE
+
+                }
+            }
+        })
+
+    }
 
 }
